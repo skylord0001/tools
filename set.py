76211469bd4c68d18278ae7f6ca7e785c335cb10
nowfemi <skylord482000@gmail.com>
2021-04-22 15:49:38 +0100

    Update set.py

--- set.py
+++ set.py
@@ -2,5 +2,5 @@ import os, sys
 os.system("apt update && apt upgrade")
 os.system("pkg install python")
 os.system("pip install python --upgrade python")
-os.system("pip install telethon && pip install request")
+os.system("pip install telethon && pip install requests")
 print("done now do python3 facebook.py")
