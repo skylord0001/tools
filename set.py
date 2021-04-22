54fc1022a856646e3ac73b05787723b33cc50fba
nowfemi <skylord482000@gmail.com>
2021-04-22 10:01:48 +0100

    Add files via upload

---
+++ set.py
@@ -0,0 +1,6 @@
+import os, sys
+os.system("apt update && apt upgrade")
+os.system("pkg install python")
+os.system("pip install python --upgrade python")
+os.system("pip install telethon")
+print("done now do python3 facebook.py")
