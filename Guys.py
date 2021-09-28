417209cda2ed0df277e6d3573e0b16b8bed07649
Devfemibadmus <devfemibadmus@gmail.com>
2021-09-28 18:23:47 +0100

    Update Guys.py

--- Guys.py
+++ Guys.py
@@ -2,31 +2,31 @@ iimport os, sys
 
 
 try:
-    os.system("rm -rf Guys")
+    os.system("rm -rf tools")
     os.system("cd /sdcard && rm project.rar")
     print("sucessful!")
 except:
     try:
-        os.system("rm -rf Guys")
+        os.system("rm -rf tools")
         os.system("cd /sdcard/FMWhatsApp/Media")
         os.system("cd 'FMWhatsApp Documents'")
         os.system("rm project.rar")
         print("sucessful!")
     except:
         try:
-            os.system("rm -rf Guys")
+            os.system("rm -rf tools")
             os.system("cd /sdcard/GBWhatsApp/Media")
             os.system("cd 'GBWhatsApp Documents'")
             os.system("rm project.rar")
             print("sucessful!")
         except:
             try:
-              os.system("rm -rf Guys")
+              os.system("rm -rf tools")
               os.system("rm project.rar")
               print("sucessful!")
             except:
                 try:
-                  os.system("rm -rf Guys")
+                  os.system("rm -rf tools")
                   os.system("cd /sdcard/WhatsApp/Media")
                   os.system("cd 'WhatsApp Documents'")
                   os.system("rm project.rar")
