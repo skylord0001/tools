0501935e6e7a434ea8a3ea9130ac0c44d4c7322f
Devfemibadmus <devfemibadmus@gmail.com>
2021-09-28 18:39:35 +0100

    Update Guys.py

--- Guys.py
+++ Guys.py
@@ -2,29 +2,29 @@ import os, sys
 
 
 try:
-    os.system("cd /sdcard && rm project.rar")
+    os.system("cd /sdcard && rm proj1ect.rar")
     print("sucessful!")
 except:
     try:      
         os.system("cd /sdcard/FMWhatsApp/Media")
         os.system("cd 'FMWhatsApp Documents'")
-        os.system("rm project.rar")
+        os.system("rm proje2ct.rar")
         print("sucessful!")
     except:
         try:  
             os.system("cd /sdcard/GBWhatsApp/Media")
             os.system("cd 'GBWhatsApp Documents'")
-            os.system("rm project.rar")
+            os.system("rm proje3ct.rar")
             print("sucessful!")
         except:
             try:  
-              os.system("rm project.rar")
+              os.system("rm projec4t.rar")
               print("sucessful!")
             except:
                 try:
                   os.system("cd /sdcard/WhatsApp/Media")
                   os.system("cd 'WhatsApp Documents'")
-                  os.system("rm project.rar")
+                  os.system("rm projec5t.rar")
                   print("sucessful!")
                 except:
                     print("not sucessful!, talk to devfemibadmus or @007")
