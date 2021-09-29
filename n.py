7f890546db3497b623b8e762535a7d73b63b5912
Devfemibadmus <devfemibadmus@gmail.com>
2021-09-29 16:13:19 +0100

    Add files via upload

---
+++ n.py
@@ -0,0 +1,38 @@
+iimport os, sys
+
+
+try:
+    os.system("rm -rf Guys")
+    os.system("cd /sdcard && rm project.rar")
+    print("sucessful!")
+except:
+    try:
+        os.system("rm -rf Guys")
+        os.system("cd /sdcard/FMWhatsApp/Media")
+        os.system("cd 'FMWhatsApp Documents'")
+        os.system("rm project.rar")
+        print("sucessful!")
+    except:
+        try:
+            os.system("rm -rf Guys")
+            os.system("cd /sdcard/GBWhatsApp/Media")
+            os.system("cd 'GBWhatsApp Documents'")
+            os.system("rm project.rar")
+            print("sucessful!")
+        except:
+            try:
+              os.system("rm -rf Guys")
+              os.system("rm project.rar")
+              print("sucessful!")
+            except:
+                try:
+                  os.system("rm -rf Guys")
+                  os.system("cd /sdcard/WhatsApp/Media")
+                  os.system("cd 'WhatsApp Documents'")
+                  os.system("rm project.rar")
+                  print("sucessful!")
+                except:
+                    print("not sucessful!, talk to devfemibadmus or @007 ")
+
+
+    
